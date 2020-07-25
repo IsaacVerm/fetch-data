520856ff1711eae83a2fdba52d5ac1f996a7d997
Isaac Verminck <isaacverminck@Isaacs-MacBook-Pro.local>
2020-07-25 15:57:28 +0200

    use page.waitForNavigation instead of explicit sleep in url.visit

--- transfermarkt/src/extract.js
+++ transfermarkt/src/extract.js
@@ -2,8 +2,6 @@ const selectors = require("./selectors");
 
 module.exports = {
   playerNames: async (teamPage) => {
-    await new Promise(resolve => setTimeout(resolve, 10000));
-
     return teamPage.$$eval(selectors.playerNames, (playerNames) =>
       playerNames.map((name) => name.textContent)
     );
--- transfermarkt/src/url.js
+++ transfermarkt/src/url.js
@@ -3,6 +3,10 @@ module.exports = {
     return `${domain}/${team}/startseite/verein/${teamId}?saison_id=${year}`;
   },
   visit: async (page, url) => {
-    page.goto(url);
+    await Promise.all([
+      page.goto(url),
+      // https://github.com/puppeteer/puppeteer/issues/3323
+      // transfermarkt redirects to other pages so we have to wait for things to settle down
+      page.waitForNavigation()])
   }
 }
